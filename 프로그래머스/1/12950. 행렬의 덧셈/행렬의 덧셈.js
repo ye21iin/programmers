d779ede47df040fe23c2b59f5b6f44aa8e9b78e7
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2025-01-06 23:53:30 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.13 ms, Memory: 67.3 MB -BaekjoonHub

--- 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -1,13 +1,5 @@
 function solution(arr1, arr2) {
-    var answer = [[]];
-    for(let i=0;i<arr1.length;i++){
-        answer[i] = [];
-        for (let j=0;j<arr1[i].length;j++){
-            let a = arr1[i][j];
-            let b = arr2[i][j];
-            let temp = a+b;
-            answer[i][j] = temp;
-        }
-    }
-    return answer;
+    return arr1.map((row, i) => 
+        row.map((value, j) => value + arr2[i][j])
+    );
 }
