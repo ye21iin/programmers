862fa16442fe0ce836fda7fe324fa9dde1efd009
Yelin Kim <97957438+ye21iin@users.noreply.github.com>
2025-01-29 20:48:55 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.js
@@ -0,0 +1,11 @@
+function solution(a, b, c) {
+    if(a === b && b === c){
+        return (a+b+c)*(a**2+b**2+c**2)*(a**3+b**3+c**3);
+    }
+    if(a === b && b !==c || a !== b && b === c || a === c && b !== c){
+        return (a+b+c)*(a**2+b**2+c**2);
+    }
+    else{
+        return a+b+c;
+    }
+}
